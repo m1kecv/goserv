f989b20e1c4a896d393a5454dc7e7dd2afb9c9b9
Mike Shemanskiy <mike.shemanskiy@gmail.com>
2024-09-16 17:47:08 +0300

    l7

--- main.go
+++ main.go
@@ -31,6 +31,7 @@ func main() {
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
 	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGet)
